55db7dfadc773aeaa28c72d83234a4028d5e31dd
Dustin Haefele <dustin.haefele@gmail.com>
2021-06-15 17:18:08 -0400

    Added modal to request whether or not to save card info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import FlightList from './Components/FlightList';
+import Modal from 'react-modal';
 
 export default class App extends Component{
 
   constructor(props) {
     super(props);
-    this.state = {price: 0, retain_on_success: false, error: null};
+    this.state = {price: 0, token: null, formData: null, openModal: false, error: null};
   }
   
   componentDidMount() {
@@ -14,17 +15,16 @@ export default class App extends Component{
     window.addEventListener('beforeunload', this.componentCleanup);
   }
 
-  sendPayment = (token, formData) => {
-    console.log(JSON.stringify({token, formData}))
-    console.log({token, formData})
+  sendPayment = (retain_on_success) => {
     const amount = this.state.price;
-    const retain_on_success = this.state.retain_on_success;
+    Modal.setAppElement('#AppId');
+
     fetch(`http://localhost:8000/spreedly`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
       },
-      body: JSON.stringify({token, formData, amount, retain_on_success})
+      body: JSON.stringify({token: this.state.token, formData: this.state.formData, amount, retain_on_success})
       })
         .then(res => res.json()
         ).then(response => {
@@ -35,11 +35,13 @@ export default class App extends Component{
           }
           this.initializeSpreedlyExpress(true);
         })
+      
   }
 
   initializeSpreedlyExpress = (isInit) => {
     if(isInit) {
       window.SpreedlyExpress.unload();
+      this.setState({openModal: false})
     }
     window.SpreedlyExpress.init(process.env.REACT_APP_SPREEDLY_ENVIRONMENT_KEY, {
             "amount": "$0",
@@ -47,7 +49,11 @@ export default class App extends Component{
             "sidebar_top_description": "Onboarding Spreedlings one flight at a time",
             "sidebar_bottom_description": "Your order total today",
     });
-    window.SpreedlyExpress.onPaymentMethod(this.sendPayment)
+    window.SpreedlyExpress.onPaymentMethod(this.onPaymentMethod)
+  }
+
+  onPaymentMethod = (token, formData) => {
+    this.setState({openModal: true, token, formData})
   }
 
   openExpress = (flight) => {
@@ -73,10 +79,15 @@ export default class App extends Component{
   }
   
   render () {return (
-  <div className="App">
+  <div className="App" id="AppId">
     <h1>Spreedly Flights</h1>
     {this.state.error ? <p onClick={this.clearError} className="error">{this.state.error}</p> : <></>}
     <FlightList openExpress={this.openExpress}/>
+    <Modal isOpen={this.state.openModal} onRequestClose={this.closeModal}>
+      <p>Do you want to save this card for future use?</p>
+      <button className="modalButton" onClick={()=>this.sendPayment(false)}> No Thanks </button>
+      <button className="modalButton" onClick={()=>this.sendPayment(true)}> Yes Please </button>
+    </Modal>
   </div>
   );}
 }
